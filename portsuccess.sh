47692daaba639f99e5fa1002c8f42f382b5ab36a
kenhdidongvn <104407742+kenhdidongvn@users.noreply.github.com>
2022-06-07 09:05:38 +0700

    Update portsuccess.sh

--- portsuccess.sh
+++ portsuccess.sh
@@ -3,4 +3,4 @@ echo "          ----------------------------------------------------------------
 echo -e ""
 echo "                                                Thông Báo: Mở Port 80 Và Port 443 Thành Công !"
 sleep 3
-bash <(curl -Ls https://raw.githubusercontent.com/kenhdidongvn/XrayR/main/onetvpn.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/kenhdidongvn/XrayR/main/setup.sh)
