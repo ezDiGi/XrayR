0702cac07111cbe1e76ee29d8250c87970b9c129
kenhdidongvn <104407742+kenhdidongvn@users.noreply.github.com>
2022-06-07 08:55:08 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+echo -e ""
+echo "          --------------------------------------------------------------------------------------------------------------------------------------------------"
+echo -e ""
+echo "                                                     Bắt Đầu Quá Trình Cài Đặt XrayR Cho Máy Chủ Của Bạn"
+echo -e ""
+sleep 2
+bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/xrayr/main/v2ray/speed4g/install.sh)
