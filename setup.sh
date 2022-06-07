5a40e94f6f5300fbb0e822a93c6c879aa5df041b
kenhdidongvn <104407742+kenhdidongvn@users.noreply.github.com>
2022-06-07 09:15:08 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 echo -e ""
-echo "          --------------------------------------------------------------------------------------------------------------------------------------------------"
+echo " --------------------------------------------------"
 echo -e ""
-echo "                                                     Bắt Đầu Quá Trình Cài Đặt XrayR Cho Máy Chủ Của Bạn"
+echo " Bắt Đầu Quá Trình Cài Đặt XrayR Cho Máy Chủ Của Bạn"
 echo -e ""
 sleep 2
 bash <(curl -Ls https://raw.githubusercontent.com/kenhdidongvn/XrayR/main/onetvpn.sh)
