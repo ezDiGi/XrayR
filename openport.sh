0380a57a7acfc8f9a497a93216672777668a9d9b
kenhdidongvn <104407742+kenhdidongvn@users.noreply.github.com>
2022-06-07 08:55:46 +0700

    Update openport.sh

--- openport.sh
+++ openport.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ufw allow 80/tcp && ufw allow 443/tcp && ufw allow 80 && ufw allow 443
 rm -rf $0
-bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/allowportsuccess.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/kenhdidongvn/XrayR/main/portsuccess.sh)
