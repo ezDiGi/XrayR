36aa0c447bd3451ef266bd78511af7f5f6a9e3c7
kenhdidongvn <104407742+kenhdidongvn@users.noreply.github.com>
2022-06-07 09:14:44 +0700

    Update portsuccess.sh

--- portsuccess.sh
+++ portsuccess.sh
@@ -1,6 +1,6 @@
 echo -e ""
-echo "          --------------------------------------------------------------------------------------------------------------------------------------------------"
+echo "----------------------------------------------------------------------"
 echo -e ""
-echo "                                                Thông Báo: Mở Port 80 Và Port 443 Thành Công !"
+echo "Thông Báo: Mở Port 80 Và Port 443 Thành Công !"
 sleep 3
 bash <(curl -Ls https://raw.githubusercontent.com/kenhdidongvn/XrayR/main/setup.sh)
