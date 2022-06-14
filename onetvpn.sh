e0fe14a317d64d741ea44e2f2fc374be34de890a
ezDiGiDigital <104407742+ezDiGi@users.noreply.github.com>
2022-06-14 19:38:19 +0700

    Update onetvpn.sh

--- onetvpn.sh
+++ onetvpn.sh
@@ -141,7 +141,7 @@ config_docker() {
 version: '3'
 services: 
   xrayr: 
-    image: misakano7545/xrayr:latest
+    image: crackair/xrayr:latest
     volumes:
       - ./config.yml:/etc/XrayR/config.yml # thư mục cấu hình bản đồ
       - ./dns.json:/etc/XrayR/dns.json 
