c930d1151695705b72d25d0e9bcd3729e26e4b38
ezDiGiDigital <104407742+ezDiGi@users.noreply.github.com>
2022-06-14 19:29:55 +0700

    Update openport.sh

--- openport.sh
+++ openport.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 ufw allow 80/tcp && ufw allow 443/tcp && ufw allow 80 && ufw allow 443
-rm -rf $0
 bash <(curl -Ls https://raw.githubusercontent.com/kenhdidongvn/XrayR/main/portsuccess.sh)
