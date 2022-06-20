045252cbbd918014d59234fa8447f707b226b613
ezDiGiDigital <104407742+ezDiGi@users.noreply.github.com>
2022-06-20 17:00:36 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ echo -e ""
 echo " Bắt Đầu Quá Trình Cài Đặt XrayR Cho Máy Chủ Của Bạn"
 echo -e ""
 sleep 2
-bash <(curl -Ls https://raw.githubusercontent.com/kenhdidongvn/XrayR/main/onetvpn.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/kenhdidongvn/XrayR/main/onetvpnxyz.sh)
